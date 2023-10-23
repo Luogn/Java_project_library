75dadc53f763117055db5f4c96f26a91080e24d6
Ho_Nguyen_Luong <22024515@vnu.edu.vn>
2023-10-23 14:34:31 +0700

    change some tiny feature

--- src/main/java/Controller/SceneController.java
+++ src/main/java/Controller/SceneController.java
@@ -21,6 +21,15 @@ public class SceneController {
         stage.show();
     }
 
+    public void swichToScene3(ActionEvent event) throws Exception {
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("/Controller/GameController.fxml"));
+        Parent root = fxml.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void swichToScene4(ActionEvent event) throws Exception {
         FXMLLoader fxml = new FXMLLoader(getClass().getResource("/Controller/TranslationController.fxml"));
         Parent root = fxml.load();
