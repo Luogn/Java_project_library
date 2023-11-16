55454d23f008a091fdfacb26de658f929822b0b3
Lưu Quang Khải <toilaquangkhai@gmail.com>
2023-11-16 21:09:55 +0700

    cập nhật googletranslate

--- src/main/java/Controller/TranslationController.java
+++ src/main/java/Controller/TranslationController.java
@@ -6,13 +6,64 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class TranslationController {
     @FXML
     TextArea targetArea;
     @FXML
     TextArea meaningArea;
+    String texten;
+
+    private static String translate(String langFrom, String langTo, String text) throws IOException {
+        String urlStr = "https://script.google.com/macros/s/AKfycbxzCfT78zpe2hPNd75uEzo7Joq2m-ach6UxoEuKpnf0JEgUcBc4C7SKL6QFeZ5ghDJM/exec"
+                +
+                "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public  void  translateEnToVi() {
+        try {
+            texten = targetArea.getText();
+            String a= translate("en", "vi", texten);
+            meaningArea.setText(a);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void  translateViToEn() {
+        try {
+            texten = meaningArea.getText();
+            String a= translate("vi", "en", texten);
+            targetArea.setText(a);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
