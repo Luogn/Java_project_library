3152b2618d0a180b051c3eb87672f68eef702f9e
Ho_Nguyen_Luong <22024515@vnu.edu.vn>
2023-11-23 00:12:25 +0700

    sửa bookmark và dictionary

--- src/main/java/Controller/WordMarkController.java
+++ src/main/java/Controller/WordMarkController.java
@@ -7,4 +7,6 @@ import java.util.ArrayList;
 public class WordMarkController {
     public static ArrayList<String> searchHistory = new ArrayList<>();
     public static ArrayList<String> bookMark = new ArrayList<>();
+
+    public static ArrayList<String> bookMark123 = new ArrayList<>();
 }
