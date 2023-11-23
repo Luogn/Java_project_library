1b461802ffb304410e8823922319300b61cb40a6
Lưu Quang Khải <toilaquangkhai@gmail.com>
2023-11-23 23:57:31 +0700

    add history

--- src/main/java/Controller/DictionaryController.java
+++ src/main/java/Controller/DictionaryController.java
@@ -4,12 +4,16 @@ package Controller;
 import CommandLine.Dictionary;
 import CommandLine.DictionaryManagement;
 import CommandLine.Word;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.io.*;
 import java.net.URL;
 import java.util.Objects;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -30,8 +34,12 @@ public class DictionaryController implements Initializable {
     private Button bookMark;
 //    private final ImageView imgViewMark = new ImageView(new Image(Objects.requireNonNull
 //            (getClass().getResourceAsStream("/Icon/star.png"))));
+    private Button buttonhistory;
+    @FXML
+    public ListView<String> history ;
 
-    public void search() {
+    public ArrayList<String> historyword = new ArrayList<>();
+    public void search() throws Exception {
         // Chỉ hiển thị listView khi bắt đầu search
         if(!(my_listView.isVisible())) {
             my_listView.setVisible(true);
@@ -55,6 +63,30 @@ public class DictionaryController implements Initializable {
                 String newStr = DictionaryManagement.dictionaryLookup(my_listView.getSelectionModel().getSelectedItem());
                 my_textarea.setText(newStr);
             }
+
+        });
+        my_listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                //////////đọc vào file history
+                FileWriter fileWriter = null;
+                try {
+                    fileWriter = new FileWriter("src\\main\\resources\\Neccessary\\historyword.txt", true);
+
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                    bufferedWriter.write(newValue);
+                    bufferedWriter.newLine();
+                    bufferedWriter.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        history.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                String newStr = DictionaryManagement.dictionaryLookup(history.getSelectionModel().getSelectedItem());
+                my_textarea.setText(newStr);
+            }
         });
     }
 
@@ -67,4 +99,30 @@ public class DictionaryController implements Initializable {
         String word = my_textfield.getText();
         WordMarkController.bookMark.add(word);
     }
-}
+
+    public void Historyword() throws IOException {
+        ObservableList<String> dataList = FXCollections.observableArrayList();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("src\\main\\resources\\Neccessary\\historyword.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        {
+            String line;
+            while (true) {
+                try {
+                    if (!((line = reader.readLine()) != null)) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                dataList.add(line);
+            }
+            reader.close();
+            history.getItems().setAll(dataList);
+            history.setVisible(!history.isVisible());
+
+
+        }
+    }}
